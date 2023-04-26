6437378ce4fa30f134e083a78a9496e758e218d2
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2023-04-26 18:03:48 +0200

    corregi el fancybox

--- Zlider.js
+++ Zlider.js
@@ -109,5 +109,7 @@ window.addEventListener('resize', updateThumbnails);
 // Establece el valor inicial de la variable
 handleSizeChange(mediaQuery);
 
-
+Fancybox.bind("[data-fancybox]", {
+  // Options
+});
 }
